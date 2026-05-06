ac92e5afa9b167024c6abe105f481da2449970f2
atmataapp-afk <atmataapp@gmail.com>
2026-05-06 12:44:37 +0300

    Update view_appointments.php

--- view_appointments.php
+++ view_appointments.php
@@ -26,7 +26,7 @@ $appointments = $stmt->fetchAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>سجل المواعيد الذكي</title>
+    <title>سجل المواعيد </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
